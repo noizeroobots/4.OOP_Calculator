fabc10aab8bd98e222569efd04abbad9b8ef7c10
aleksey <aleksei.dolgov@simbirsoft.com>
2022-02-25 11:05:30 +0300

    Добавил маску для ввода выражения расчёта

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.println("Enter the expression: ");
+        System.out.println("Enter the expression: (mask: '10+10') ");
         String expression = in.nextLine();
         String f = "f";
 
